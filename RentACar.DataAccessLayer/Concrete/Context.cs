5b04bf41c2cb0989d7d2a9edd552f4a385fbd6d9
Hamza Ertugrul Kuru <hertugrulkuru@gmail.com>
2023-07-04 21:09:48 +0300

    Add project files.

---
+++ RentACar.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentACar.EntityLayer.Concrete;
+
+namespace RentACar.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        //Pluralize
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
